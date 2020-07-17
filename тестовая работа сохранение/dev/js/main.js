243e5e8ac2e0036a623dbfbd5f56168cbf3357de
farad233 <farad233@gmail.com>
2020-07-17 14:08:50 +0300

    test_project

--- тестовая работа сохранение/dev/js/main.js
+++
@@ -1,9 +0,0 @@
-inputName = document.getElementById('name'),addEventListener('input',
-(   {target:  {value} }) => {
-    console.log(value)
-});
-
-inputTel = document.getElementById('telephone'),addEventListener('input',
-(   {target:  {value} }) => {
-    console.log(value)
-});
